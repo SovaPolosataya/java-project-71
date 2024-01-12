ba563256c526ebe772b56a9f1b0553b6c8c34f46
Irina Yukavskaya <strix1813@yandex.ru>
2024-01-12 21:05:17 +0300

    update App.java

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,41 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and show a difference.")
+
+class App implements Callable<Integer> {
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(paramLabel = "filepath1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
+    String format;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    static boolean help;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean version;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        //var diff = Differ.generate(filepath1, filepath2);
+        //System.out.println(diff);
+        return 0;
+    }
+}
