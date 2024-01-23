83ddc1511dcd9f1ef5bd8f1251bd494df1fdb793
Irina Yukavskaya <strix1813@yandex.ru>
2024-01-24 02:42:41 +0300

    added final variables

--- app/src/main/java/hexlet/code/DiffUtils.java
+++ app/src/main/java/hexlet/code/DiffUtils.java
@@ -7,11 +7,19 @@ import java.util.stream.Stream;
 
 
 public class DiffUtils {
-    public static Map<String, String> diffTest(Map isMap1, Map isMap2) throws Exception {
+
+    public static final String ADDED = "  + ";
+    public static final String DELETED = "  - ";
+    public static final String UPDATED = "  + ";
+    public static final String UNMODIFIED = "    ";
+
+    public static Map<String, String> comparator(Map isMap1, Map isMap2) throws Exception {
 
         Map result = new LinkedHashMap<String, String>();
-        var keys1 = new ArrayList<String>(isMap1.keySet());
-        var keys2 = new ArrayList<String>(isMap2.keySet());
+        var keys1 = new ArrayList<String>();
+        var keys2 = new ArrayList<String>();
+        keys1.addAll(isMap1.keySet());
+        keys2.addAll(isMap2.keySet());
         var sortedKeys = Stream.concat(keys1.stream(), keys2.stream()).sorted().toList();
 
         for (var key : sortedKeys) {
@@ -19,14 +27,14 @@ public class DiffUtils {
             var value2 = isMap2.get(key);
 
             if (!isMap1.containsKey(key)) {
-                result.put("+ " + key, value2);
+                result.put(ADDED + key, value2);
             } else if (!isMap2.containsKey(key)) {
-                result.put("- " + key, value1);
+                result.put(DELETED + key, value1);
             } else if (value1.equals(value2)) {
-                result.put("  " + key, value1);
+                result.put(UNMODIFIED + key, value1);
             } else {
-                result.put("- " + key, value1);
-                result.put("+ " + key, value2);
+                result.put(DELETED + key, value1);
+                result.put(UPDATED + key, value2);
             }
         }
         return result;
