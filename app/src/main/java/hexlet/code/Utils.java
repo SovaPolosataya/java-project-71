ba063f8dc13e21f16c8296681b3e705fe52b60cd
Irina Yukavskaya <strix1813@yandex.ru>
2024-01-24 02:44:02 +0300

    add Utils

---
+++ app/src/main/java/hexlet/code/Utils.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class Utils {
+    public static String getFileFormat(String filePath) throws Exception {
+        String format;
+
+        int i = filePath.lastIndexOf('.');
+        format = i > 0 ? filePath.substring(i + 1) : "";
+        return format;
+    }
+
+    public static String getFilePath(String filePath) throws Exception {
+        Path path;
+
+        if (!filePath.startsWith("./")) {
+            path = Paths.get("src/test/resources/", filePath).toAbsolutePath().normalize();
+        } else {
+            path = Paths.get(filePath).toAbsolutePath().normalize();
+        }
+
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist");
+        }
+
+        String content = Files.readString(path);
+        return content;
+    }
+    //public static void changesProcessing(Map result) {
+}
