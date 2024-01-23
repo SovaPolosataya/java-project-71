97ec95f8a2a425c7194bbf863da98e5c20b91d88
Irina Yukavskaya <strix1813@yandex.ru>
2024-01-24 02:39:55 +0300

    added test

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -6,17 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDiffer {
     @Test
-    public void testGenerate() throws Exception {
+    public void testGenerateJson() throws Exception {
         String testResult = "{\n"
-                          +  "- follow: false\n"
-                          +  "  host: hexlet.io\n"
-                          +  "- proxy: 123.234.53.22\n"
-                          +  "- timeout: 50\n"
-                          +  "+ timeout: 20\n"
-                          +  "+ verbose: true\n"
+                          +  "  - follow: false\n"
+                          +  "    host: hexlet.io\n"
+                          +  "  - proxy: 123.234.53.22\n"
+                          +  "  - timeout: 50\n"
+                          +  "  + timeout: 20\n"
+                          +  "  + verbose: true\n"
                           +  "}";
         String filePath1 = "file1.json";
         String filePath2 = "file2.json";
+        assertEquals(testResult, Differ.generate(filePath1, filePath2));
+    }
+
+    @Test
+    public void testGenerateYml() throws Exception {
+        String testResult = "{\n"
+                +  "  - follow: false\n"
+                +  "    host: hexlet.io\n"
+                +  "  - proxy: 123.234.53.22\n"
+                +  "  - timeout: 50\n"
+                +  "  + timeout: 20\n"
+                +  "  + verbose: true\n"
+                +  "}";
+        String filePath1 = "file1.yml";
+        String filePath2 = "file2.yml";
         assertEquals(testResult, Differ.generate(filePath1, filePath2));
     }
 }
