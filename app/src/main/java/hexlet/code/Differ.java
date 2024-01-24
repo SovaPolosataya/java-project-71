65d5af046b8cce8e9fc5819a52237dbdcc6beb95
Irina Yukavskaya <strix1813@yandex.ru>
2024-01-24 17:29:25 +0300

    update generate()

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,14 +2,13 @@ package hexlet.code;
 
 import java.util.Map;
 
-
 import static hexlet.code.Parser.parser;
 import static hexlet.code.Utils.getFileFormat;
 import static hexlet.code.Utils.getFilePath;
 
 
 public class Differ {
-    public static String generate(String filePath1, String filePath2) throws Exception {
+    public static String generate(String filePath1, String filePath2, String format) throws Exception {
         String path1 = getFilePath(filePath1);
         String path2 = getFilePath(filePath2);
 
@@ -17,8 +16,11 @@ public class Differ {
         var mapFile2 = parser(path2, getFileFormat(filePath2));
 
         Map<String, String> resultMap = DiffUtils.comparator(mapFile1, mapFile2);
-        String result = DiffUtils.stringOfMap(resultMap);
+        String result = Utils.changesProcessing(resultMap, format);
 
         return result;
+    }
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, "stylish");
     }
 }
