cdc676f3215ac1ee10a6b835daacadba8c12ae09
Irina Yukavskaya <strix1813@yandex.ru>
2024-02-10 00:44:34 +0300

    add new comparator

--- app/src/main/java/hexlet/code/DiffComparator.java
+++ app/src/main/java/hexlet/code/DiffComparator.java
@@ -1,40 +1,47 @@
 package hexlet.code;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Objects;
 
 public class DiffComparator {
 
-    public static final String ADDED = "ADDED**";
-    public static final String DELETED = "DELETED**";
-    public static final String REPLACEMENT = "REPLACEMENT**";
-    public static final String UNMODIFIED = "UNMODIFIED**";
-    public static final String CHANGED = "CHANGED**";
+    public static final String ADDED = "ADDED";
+    public static final String DELETED = "DELETED";
+    public static final String UNMODIFIED = "UNMODIFIED";
+    public static final String CHANGED = "CHANGED";
+    public static final String STATUS = "STATUS";
+    public static final String KEY = "KEY";
+    public static final String VALUE = "VALUE";
+    public static final String NEW_VALUE = "newVALUE";
 
-    public static Map<String, String> comparator(Map map1, Map map2) throws Exception {
+    public static List<Map<String, Object>> comparator(Map map1, Map map2) throws Exception {
+        List<Map<String, Object>> result = new LinkedList<>();
 
-        Map result = new LinkedHashMap<String, String>();
-        var keys1 = new ArrayList<String>();
-        var keys2 = new ArrayList<String>();
-        keys1.addAll(map1.keySet());
-        keys2.addAll(map2.keySet());
-        var sortedKeys = Stream.concat(keys1.stream(), keys2.stream()).sorted().toList();
+        Map<String, Object> allKeys = new HashMap<>();
+        allKeys.putAll(map1);
+        allKeys.putAll(map2);
+        List<String> sortedKeys = allKeys.keySet().stream()
+                                            .sorted()
+                                            .toList();
 
         for (var key : sortedKeys) {
-            var value1 = valueNull(map1.get(key));
-            var value2 = valueNull(map2.get(key));
+            Object value1 = valueNull(map1.get(key));
+            Object value2 = valueNull(map2.get(key));
 
             if (!map1.containsKey(key)) {
-                result.put(ADDED + key, value2);
+                result.add(getMapForDiff(ADDED, key, value2));
             } else if (!map2.containsKey(key)) {
-                result.put(DELETED + key, value1);
-            } else if (value1.equals(value2)) {
-                result.put(UNMODIFIED + key, value1);
-            } else {
-                result.put(CHANGED + key, value1);
-                result.put(REPLACEMENT + key, value2);
+                result.add(getMapForDiff(DELETED, key, value1));
+            } else if (map1.containsKey(key) && map2.containsKey(key)) {
+                if (Objects.equals(value1, value2)) {
+                    result.add(getMapForDiff(UNMODIFIED, key, value1));
+                } else {
+                    result.add(getMapForDiff(CHANGED, key, value1, value2));
+                }
             }
         }
         return result;
@@ -45,5 +52,26 @@ public class DiffComparator {
             value = "null";
         }
         return value;
+    }
+
+    public static Map<String, Object> getMapForDiff(String status, String key, Object value1, Object value2) {
+        Map<String, Object> mapForResult = new LinkedHashMap<>();
+
+        mapForResult.put(STATUS, status);
+        mapForResult.put(KEY, key);
+        mapForResult.put(VALUE, value1);
+        mapForResult.put(NEW_VALUE, value2);
+
+        return mapForResult;
+    }
+
+    public static Map<String, Object> getMapForDiff(String status, String key, Object value) {
+        Map<String, Object> mapForResult = new LinkedHashMap<>();
+
+        mapForResult.put(STATUS, status);
+        mapForResult.put(KEY, key);
+        mapForResult.put(VALUE, value);
+
+        return mapForResult;
     }
 }
