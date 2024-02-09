390a5085c20744ca954d069398ba24176d09312f
Irina Yukavskaya <strix1813@yandex.ru>
2024-02-10 00:52:55 +0300

    update Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,20 +1,22 @@
 package hexlet.code;
 
+import java.util.List;
 import java.util.Map;
 
 import static hexlet.code.Parser.parser;
 import static hexlet.code.Utils.getFileFormat;
 import static hexlet.code.Utils.getFilePath;
+import static hexlet.code.Utils.readFile;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        String path1 = getFilePath(filePath1);
-        String path2 = getFilePath(filePath2);
+        String fileContent1 = readFile(getFilePath(filePath1));
+        String fileContent2 = readFile(getFilePath(filePath2));
 
-        var mapFile1 = parser(path1, getFileFormat(filePath1));
-        var mapFile2 = parser(path2, getFileFormat(filePath2));
+        var mapFile1 = parser(fileContent1, getFileFormat(filePath1));
+        var mapFile2 = parser(fileContent2, getFileFormat(filePath2));
 
-        Map<String, String> resultMap = DiffComparator.comparator(mapFile1, mapFile2);
+        List<Map<String, Object>> resultMap = DiffComparator.comparator(mapFile1, mapFile2);
         String result = Formatter.choiceOfStyle(resultMap, format);
 
         return result;
